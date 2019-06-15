098f389e15d2f8ec6ada6635bbaa84deb7eca877
Grant Van Horn <grant.t.vanhorn@gmail.com>
2019-06-15 14:01:17 -0400

    * Update server to have examples of custom validation via models, resolvers and custom error messages

--- src/index.js
+++ src/index.js
@@ -15,9 +15,20 @@ app.use(cors());
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  context: {
-    models,
+  formatErrors: error => {
+    const message = error.message
+      .replace('SequelizeValidationError: ', '')
+      .replace('Validation error: ', '');
+
+    return {
+      ...error,
+      message,
+    };
   },
+  context: async () => ({
+    models,
+    me: await models.User.findByLogin('gtest1'),
+  }),
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -7,6 +7,7 @@ export default {
       return await models.User.findById(id);
     },
     me: async (parent, args, { models, me }) => {
+      if (!me) return null;
       return await models.User.findById(me.id);
     },
   },
