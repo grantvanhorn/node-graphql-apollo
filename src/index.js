1e2d2b3e955d0c8fcdc3f550337eafcfd219af37
Grant Van Horn <grant.t.vanhorn@gmail.com>
2019-06-15 12:52:16 -0400

    * Update server to create seed data with user and message models

--- src/index.js
+++ src/index.js
@@ -22,8 +22,47 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-sequelize.sync().then(async () => {
+const eraseDatabaseOnSync = true;
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+
   app.listen({ port: 8000}, () => {
     console.log('Apollo Server on http://localhost:8000/graphql');
   });
 });
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'gtest1',
+      messages: [
+        {
+          text: 'This is a seeded message from server start'
+        }
+      ]
+    },
+    {
+      include: [models.Message],
+    },
+  );
+
+  await models.User.create(
+    {
+      username: 'ddavids',
+      messages: [
+        {
+          text: 'Happy to release ...',
+        },
+        {
+          text: 'Published a complete ...',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+}
